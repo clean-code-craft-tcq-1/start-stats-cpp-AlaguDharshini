2c314c9c3cb312735f585acb60c8a5ffc985219f
AlaguDharshini <77431474+AlaguDharshini@users.noreply.github.com>
2021-01-28 18:57:53 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,21 @@
 #include "stats.h"
-
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+struct stat
+{
+    int min,max;
+    float average;
+};
+struct stat myval;
+auto Statistics::ComputeStatistics(const std::vector<___>&vect ) {
     //Implement statistics here
+    myval.min,myval.max =vect[i];
+    myval.average =0.0;
+    for (int i=1; i<vect.size(); i++) 
+    {
+        if(vect[i] <min)
+            min =vect[i];
+        if(vect[i] >max)
+            max =vect[i];
+        (average +=vect[i])/2;
+    }
+    return myval;
 }
